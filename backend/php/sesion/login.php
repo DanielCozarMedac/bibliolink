e08fdb870a14ee4c9bd5630842ab2e129bbcbc64
DanielCozarMedac <dcr0035@alu.medac.es>
2026-03-18 20:57:01 +0100

    Modificaciones
    El HTML de inicio de sesion se integra en el php del mismo
    Se crea la conexion.php de la bbdd

--- backend/php/sesion/login.php
+++ backend/php/sesion/login.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
+    <title>Bibliolink</title>
     <?php
         error_reporting(E_ALL);
         ini_set("display_errors", 1);
